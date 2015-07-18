a1b594f2eaf00fe12a9fabf8ecb78984bb2de909
Brandan Haertel <foilpudding@gmail.com>
2015-07-17 17:45:52 -0700

    made large game, made cage, added sound, tweeked jumps

---
+++ Assets/Resources/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public GameObject Hearts;
+    public int PlayerNumber;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool GainHeart()
+    {
+        var ph = Hearts.GetComponent<PlayerHeartManager>();
+        return ph.GainHeart();
+
+    }
+}
