615ae425c44d5ba039f2e6fadcf555576a2302c6
brandann <foilpudding@gmail.com>
2015-08-06 09:27:50 -0700

    made blades, fixed player movement

---
+++ Assets/SawbladeBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawbladeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Player hit: " + coll.gameObject.tag);
+    }
+}
