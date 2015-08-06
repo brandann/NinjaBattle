c7432a1a906002c581d058a580e30cfa77abe7c5
brandann <foilpudding@gmail.com>
2015-08-06 10:04:12 -0700

    made 2 levels

--- Assets/KillPlayerOnTouch.cs
+++ Assets/KillPlayerOnTouch.cs
@@ -1,24 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SawbladeBehavior : MonoBehaviour {
+public class KillPlayerOnTouch : MonoBehaviour {
 
     private GameObject Spawnpoint;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Spawnpoint = GameObject.Find("SpawnPoint");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        
-        if(coll.gameObject.tag == "Player")
+
+        if (coll.gameObject.tag == "Player")
         {
             Debug.Log("Player hit: " + coll.gameObject.tag);
             coll.gameObject.transform.position = Spawnpoint.transform.position;
