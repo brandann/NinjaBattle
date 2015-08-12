57196354eef9dfd699fdda71dc1372bd2677352c
Brandan Haertel <foilpudding@gmail.com>
2015-08-11 20:23:00 -0700

    made arrows die

--- Assets/Resources/Scripts/KillPlayerOnTouch.cs
+++ Assets/Resources/Scripts/KillPlayerOnTouch.cs
@@ -5,27 +5,16 @@ public class KillPlayerOnTouch : MonoBehaviour {
 
     private GameObject Spawnpoint;
 
-    private float killstart;
-    private float killwait;
-    private bool holdwait;
-
     // Use this for initialization
     void Start()
     {
         Spawnpoint = GameObject.Find("SpawnPoint");
-        holdwait = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(holdwait)
-        {
-            if(killstart - Time.timeSinceLevelLoad >= killwait)
-            {
-                GameObject.Destroy(this.gameObject);
-            }
-        }
+        
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -34,11 +23,6 @@ public class KillPlayerOnTouch : MonoBehaviour {
         {
             Debug.Log("Player hit: " + coll.gameObject.tag);
             coll.gameObject.transform.position = Spawnpoint.transform.position;
-        }
-
-        if (this.tag == "projectile")
-        {
-            holdwait = true;
         }
     }
 }
--- Assets/Resources/Scripts/ProjectileBehavior.cs
+++ Assets/Resources/Scripts/ProjectileBehavior.cs
@@ -5,13 +5,40 @@ public class ProjectileBehavior : MonoBehaviour {
 
 	public float speed;
 	
+	private float killstart;
+	private float killwait;
+	private bool holdwait;
+	
 	// Use this for initialization
 	void Start () {
-	
+		holdwait = false;
+		killwait = .5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position += transform.right * speed * Time.timeScale * Time.deltaTime;
+		if(!holdwait)
+		{
+			this.transform.position += transform.right * speed * Time.timeScale * Time.deltaTime;
+		}
+		else
+		{
+			if(Time.timeSinceLevelLoad - killstart >= killwait)
+			{
+				GameObject.Destroy(this.gameObject);
+			}
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag != "projectile")
+		{
+			holdwait = true;
+			killstart = Time.timeSinceLevelLoad;
+			var rb2d = GetComponent<BoxCollider2D>();
+			rb2d.enabled = false;
+			GetComponent<Rigidbody2D>().isKinematic = true;
+		}
 	}
 }
