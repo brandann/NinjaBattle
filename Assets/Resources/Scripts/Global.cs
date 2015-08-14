eab22c50673d060998edf2f2b7a3eb3eccddc8fb
brandann <foilpudding@gmail.com>
2015-08-14 10:12:28 -0700

    removed some extra scripts

--- Assets/Resources/Scripts/Global.cs
+++ Assets/Resources/Scripts/Global.cs
@@ -5,20 +5,6 @@ public class Global : MonoBehaviour {
 
     [Header("Players")]
     public GameObject Player1;
-    public GameObject Player1Particle;
-    public GameObject Player2;
-    public GameObject Player2Particle;
-
-    [Header("Enviroment")]
-    public GameObject BackgroundPlatforms;
-    public GameObject ForegroundPlatforms;
-    public GameObject CageObjects;
-    public GameObject StartingPlatforms;
-    public GameObject Enviroment;
-
-    [Header("Score")]
-    public GameObject Player1Score;
-    public GameObject Player2Score;
 
     [Header("Goal")]
     public GameObject GoalItemObject;
@@ -49,43 +35,12 @@ public class Global : MonoBehaviour {
             switch (value)
             {
                 case (GameState.Start):
-
-                    // Manage platforms
-                    SetPlatformActive(true, false, false, false);
-                    GoalItemObject.SetActive(false);
-                    Player1Particle.SetActive(false);
-                    Player2Particle.SetActive(false);
-                    
                     break;
                 case (GameState.Game):
-
-                    // Manage platforms
-                    SetPlatformActive(false, true, true, false);
-                    GoalItemObject.SetActive(true);
-                    Player1Particle.SetActive(true);
-                    Player2Particle.SetActive(true);
-                    GoalItemObject.GetComponent<GoalItem>().Initilize();
                     break;
                 case (GameState.End):
-                    
-                    // Manage platforms
-                    SetPlatformActive(false, false, false, true);
-                    GoalItemObject.transform.position = new Vector3(0, 14, 0);
-                    GoalItemObject.GetComponent<GoalItem>().SetColliderActive(false);
-                    if (Player1Score.GetComponent<PlayerHeartManager>().GetScore() > Player2Score.GetComponent<PlayerHeartManager>().GetScore())
-                    {
-                        SetLoserPlayer(Player2, Player1Particle);
-                    }
-                    else
-                    {
-                        SetLoserPlayer(Player1, Player2Particle);
-                    }
                     break;
                 case (GameState.Credits):
-                    
-                    // Manage Platforms
-                    SetPlatformActive(false, false, false, false);
-
                     break;
             }
             currentGameState = value;
@@ -95,24 +50,6 @@ public class Global : MonoBehaviour {
             return currentGameState;
         }
     }
-
-    private void SetPlatformActive(bool Starting, bool Foreground, bool Background, bool Cage)
-    {
-        StartingPlatforms.SetActive(Starting);
-        Enviroment.SetActive(!Starting);
-        ForegroundPlatforms.SetActive(Foreground);
-        BackgroundPlatforms.SetActive(Background);
-        CageObjects.SetActive(Cage);
-    }
-
-    private void SetLoserPlayer(GameObject player, GameObject particle)
-    {
-        particle.SetActive(false);
-        player.transform.position = new Vector3(0, -2, 0);
-    }
-    #endregion
-
-    #region TimeDilation
 
     #endregion
 
--- Assets/Resources/Scripts/Player/Player.cs
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -6,37 +6,18 @@ public class Player : MonoBehaviour {
     public delegate void PlayerDeath();
     public static event PlayerDeath OnPlayerDeath;
 
-    public GameObject Hearts;
     public int PlayerNumber;
-    private bool foreground = true;
     private GameObject Spawnpoint;
 
 	// Use this for initialization
 	void Start () {
         Spawnpoint = GameObject.Find("SpawnPoint");
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
-    public bool GainHeart()
-    {
-        var ph = Hearts.GetComponent<PlayerHeartManager>();
-        return ph.GainHeart();
-    }
-
-    public bool IsForeground()
-    {
-        return foreground;
-    }
-
-    public void SetForeground(bool isforeground)
-    {
-        foreground = isforeground;
-    }
 
     public void KillPlayer()
     {
@@ -49,6 +30,4 @@ public class Player : MonoBehaviour {
     {
         gameObject.transform.position = Spawnpoint.transform.position;
     }
-
-
 }
