8c9b0ce6de3ae472f14f58375164ed80cb2c1325
brandann <foilpudding@gmail.com>
2015-08-18 12:39:42 -0700

    added simple enemy movement

---
+++ Assets/Resources/Scripts/EnemyMotion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMotion : MonoBehaviour {
+
+    int direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(transform.position.x + direction *Time.deltaTime, -3.011f, 0);
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == 14 || coll.gameObject.layer == 10)
+        {
+            direction *= -1;
+        }
+    }
+}
