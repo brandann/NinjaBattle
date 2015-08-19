d45ed81d19d01ea335f8dafc0fc937795bf6e2c1
Gary <jgmixson@uw.edu>
2015-08-19 07:14:15 -0700

    respawn now works in tests

---
+++ Assets/Level.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Level manages the of the game on a level by level basis. 
+ * 
+ * 
+ * 
+ */
+
+public class Level : MonoBehaviour {
+
+    protected Player player;
+    protected Spawn
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Basic Player Controller A").GetComponent<Player>();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
